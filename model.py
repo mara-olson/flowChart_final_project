f08091886eb7211aa3a2bec9d569fb570e5955c1
mara-olson <mara.c.olson@gmail.com>
2022-04-30 17:48:27 -0700

    Creating a Strava user function added to the seeded data functions

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ class User(db.Model):
     
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer, autoincrement=True, primaryKey=True)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     team_name = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
@@ -19,23 +19,26 @@ class User(db.Model):
     deactivated_at = db.Column(db.DateTime)
     notifications = db.Column(db.Boolean)
 
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}'
+
 
 class StravaUser(db.Model):
     """A user connected to their Strava account."""
     
     __tablename__ = "strava_users"
 
-    strava_user_id = db.Column(db.Integer, autoincrement=True, primaryKey=True)
+    strava_user_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     
-    strava_email = db.Column(db.String, db.ForeignKey("users.user_id"), nullable=False)
+    strava_email = db.Column(db.String, nullable=False)
     team_name = db.Column(db.String)
     
     created_at = db.Column(db.DateTime)
     deactivated_at = db.Column(db.DateTime)
 
-
-
+    def __repr__(self):
+        return f'<StravaUser strava_user_id={self.user_id} user_id={self.user_id}'
 
 
 class StravaActvity(db.Model):
@@ -45,7 +48,7 @@ class StravaActvity(db.Model):
     
     __tablename__ = "strava_activities"
 
-    strava_activity_id = db.Column(db.Integer, primaryKey=True) #id
+    strava_activity_id = db.Column(db.Integer, primary_key=True) #id
     strava_user_id = db.Column(db.Integer, db.ForeignKey("strava_users.strava_user_id"), nullable=False) #athlete.id
     activity_name = db.Column(db.String) #name
     activity_type = db.Column(db.String) #type
@@ -56,7 +59,8 @@ class StravaActvity(db.Model):
     suffer_score = db.Column(db.Integer) #suffer_score (total of 9 increments, I'm assuming 1-10)
     description = db.Column(db.Text) #description
 
-
+    def __repr__(self):
+        return f'<StravaActivity strava_activity_id={self.strava_activity_id} title={self.activity_name}'
 
 
 class ActivityLog(db.Model):
@@ -64,7 +68,7 @@ class ActivityLog(db.Model):
     
     __tablename__ = "activity_logs"
 
-    activity_id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
+    activity_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False) 
     activity_name = db.Column(db.String, default="Activity") 
     activity_type = db.Column(db.String, default="Run") 
@@ -77,13 +81,16 @@ class ActivityLog(db.Model):
     created_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return f'<Activity activity_id={self.activity_id} title={self.activity_name}'
+
 
 class MenseLog(db.Model):
     """A log added by the user related to their menstrual cycle."""
     
     __tablename__ = "mense_logs"
 
-    mense_id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
+    mense_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     
     flow_volume = db.Column(db.String) #Could be a string (light, medium, heavy) or could be an integer, based on that. For plotting on a graph maybe it makes sense to have an integer?
@@ -98,6 +105,8 @@ class MenseLog(db.Model):
     created_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return f'<Mense mense_id={self.mense_id}'
 
 # I think I want to actually include sx data in the period log
 class SymptomLog(db.Model):
@@ -105,7 +114,7 @@ class SymptomLog(db.Model):
     
     __tablename__ = "sx_logs"
 
-    sx_id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
+    sx_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     
     mood = db.Column(db.Boolean)
@@ -117,16 +126,42 @@ class SymptomLog(db.Model):
     created_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return f'<Symptom strava_activity_id={self.sx_id} user={self.user_id}'
+
 
 class SleepLog(db.Model):
     """A log added by the user related to their sleep."""
    
     __tablename__ = "sleep_logs"
 
-    sleep_log_id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
+    sleep_log_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     sleep_duration = db.Column(db.Integer)
     sleep_quality = db.Column(db.Integer)
     sleep_notes = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
+    def __repr__(self):
+        return f'<SleepLog sleep_log_id={self.sx_id} user={self.user_id}'
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///period", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db, db
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", port="5001", debug=True)
