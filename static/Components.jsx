75a4d076f02c7fb09a00446b52108ecfe18685fc
mara-olson <mara.c.olson@gmail.com>
2022-05-07 13:15:12 -0700

    WIP login page -- rearranged <script> for Components & App to be at end of body, and <div id-'root'> before {%block content%}

---
+++ static/Components.jsx
@@ -0,0 +1,60 @@
+//"""Components for project"""
+
+// GENERAL COMPONENTS
+
+function Card(props) {
+  return <div className="card">Displayed!</div>;
+}
+
+function Navbar(props) {
+  const { logo, brand } = props;
+
+  //   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  //   authenticate(user_id) {
+  //       if {
+  //           user_id in
+  //   }
+
+  return (
+    <nav>
+      <ReactRouterDOM.Link
+        to="/"
+        className="navbar-brand d-flex justify-content-left"
+      >
+        <img src={logo} height="30" alt="logo" />
+      </ReactRouterDOM.Link>
+
+      <section className="d-flex justify-content-left">
+        <ReactRouterDOM.NavLink
+          to="/activities"
+          activeClassName="navlink-active"
+          className="nav-link nav-item"
+        >
+          Activities
+        </ReactRouterDOM.NavLink>
+        <ReactRouterDOM.NavLink
+          to="/profile"
+          activeClassName="navlink-active"
+          className="nav-link nav-item"
+        >
+          Profile
+        </ReactRouterDOM.NavLink>
+      </section>
+    </nav>
+  );
+}
+
+// LANDING PAGE COMPONENT
+function LandingPage(props) {
+  const landingMessage =
+    "Your training periods & menstrual periods don’t happen in isolation.";
+
+  return (
+    <div>
+      <section id="landing-message" className="d-flex justify-content-center">
+        {landingMessage}
+      </section>
+    </div>
+  );
+}
